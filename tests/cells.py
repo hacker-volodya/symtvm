5e72a3ad3475c56b9b803510db3abd804b5d7e48
hacker_volodya <d3fl4t3@gmail.com>
2023-04-08 14:50:58 +0700

    Refactor tests

--- tests/cells.py
+++ tests/cells.py
@@ -1,24 +1,37 @@
 import unittest
 
-from z3 import Solver, unsat, Not
+from z3 import *
 
 from symtvm.state.exit_codes import CellUnderflow
 from symtvm.state.symcell import SymCell
 
 
-class TestSum(unittest.TestCase):
+class TestSymCell(unittest.TestCase):
+    def setUp(self) -> None:
+        self.exceptions = []
+
+        def exc_handler(exc, constrs):
+            self.exceptions.append((exc, constrs))
+
+        def assertSymTrue(*args):
+            self.assertTrue(is_true(simplify(And(*args))))
+
+        self.exc_handler = exc_handler
+        self.assertSymTrue = assertSymTrue
+
     def test_cell_ref_underflow(self):
-        exceptions = []
-
-        def exc_handler(exc, constrs, exceptions=exceptions):
-            exceptions.append((exc, constrs))
-
-        cell = SymCell.empty(exc_handler)
-        solver = Solver()
+        cell = SymCell.empty(self.exc_handler)
         non_existent_ref = cell.load_ref()
-        self.assertTrue(type(exceptions[0][0]) is CellUnderflow)
-        solver.add(*[Not(x) for x in exceptions[0][1]])
-        self.assertTrue(solver.check() == unsat)
+
+        # assert CellUnderflow is possible
+        self.assertTrue(type(self.exceptions[0][0]) is CellUnderflow)
+
+        # assert CellUnderflow always happens
+        self.assertSymTrue(*self.exceptions[0][1])
+
+    def test_empty_cell_ref_size_is_zero(self):
+        cell = SymCell.empty(self.exc_handler)
+        self.assertSymTrue(cell.ref_size() == 0)
 
 
 if __name__ == '__main__':
